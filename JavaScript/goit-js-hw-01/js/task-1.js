960df03de6133b2880fdf04d344d46c1b66a6208
Mykhailo Mavrodii <you@example.com>
2020-03-31 15:55:58 +0300

    module-11 v2.0

---
+++ JavaScript/goit-js-hw-01/js/task-1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Задание 1")
+
+const productName = "Генератор защитного поля";
+let productPrice = 1000;
+let resultString;
+
+resultString = `Выбран "${productName}", цена за штуку ${productPrice}.`;
+console.log(resultString);
+
+productPrice = 2000;
+resultString = `Выбран "${productName}", цена за штуку ${productPrice}.`;
+
+console.log(resultString);
+console.log("")
---
+++ JavaScript/goit-js-hw-01/js/task-2.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("Задание 2")
+
+const total = 100;
+const ordered = 50;
+let orderMassage;
+
+orderMassage =
+  ordered > total
+    ? "На складе недостаточно товара!"
+    : "Заказ оформлен, с Вами свяжется менеджер";
+
+    console.log(orderMassage)
+    console.log("")
