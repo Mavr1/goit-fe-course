b37ab6e18e841ff3ea3f8bdc71ccd65f6c602188
Mykhailo Mavrodii <you@example.com>
2020-04-16 09:09:47 +0300

    task 7 added

--- JavaScript/goit-js-hw-02/js/task-7.js
+++ JavaScript/goit-js-hw-02/js/task-7.js
@@ -1,24 +1,40 @@
 "use strict";
 
+const allLogins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const addLogin = function (allLogins, login) {
+  const isLoginValid = function (login) {
+    if (login.length > 3 && login.length < 16) {
+      return true;
+    }
+    return false;
+  };
 
-const isLoginValid = function(login) {
-  // твой код
-};
+  const isLoginUnique = function (allLogins, login) {
+    for (let loginIncom of allLogins) {
+      if (loginIncom === login) {
+        return false;
+      }
+    }
+    return true;
+  };
 
-const isLoginUnique = function(allLogins, login) {
-  // твой код
-};
+  if (isLoginValid(login) === false) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  }
 
-const addLogin = function(allLogins, login) {
-  // твой код
+  if (isLoginUnique(allLogins, login) === false) {
+    return "Такой логин уже используется!";
+  }
+
+  allLogins.push(login);
+  return "Логин успешно добавлен!";
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(allLogins, "Ajax")); // 'Логин успешно добавлен!'
+console.log(addLogin(allLogins, "robotGoogles")); // 'Такой логин уже используется!'
+console.log(addLogin(allLogins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(allLogins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
