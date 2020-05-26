e3441fc9dd52884fc0b5776585b2098ae32ead44
Mavr1 <mavrodiy.m.e@gmail.com>
2020-05-26 16:28:24 +0300

    corrected list update

--- JavaScript/goit-js-hw-12-countries/src/js/responseLogic.js
+++ JavaScript/goit-js-hw-12-countries/src/js/responseLogic.js
@@ -3,11 +3,13 @@ import { renderQueryCountries, renderUniqCountry, insert } from './render';
 
 export const responseListCheck = responseList => {
   if (responseList.length > 1 && responseList.length < 11) {
+    insert('.js_countries', '');
     renderQueryCountries(responseList);
     return;
   }
 
   if (responseList.length === 1) {
+    insert('.js_countries', '');
     renderUniqCountry(responseList);
     return;
   }
