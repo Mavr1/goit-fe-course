ecbef59ed6a18f33e764f945310647be62f9b61e
Mavr1 <mavrodiy.m.e@gmail.com>
2020-05-28 18:19:20 +0300

    lightbox corrected

--- JavaScript/goit-js-hw-13-image-finder/src/js/lightbox.js
+++ JavaScript/goit-js-hw-13-image-finder/src/js/lightbox.js
@@ -4,9 +4,19 @@ const galleryRef = document.querySelector('.js_gallery');
 
 const onClickLarge = e => {
   if (e.target.nodeName !== 'IMG') return;
-  const modal = basicLightbox.create(`
+  const modal = basicLightbox.create(
+    `
     <img src="${e.target.dataset.sourcelarge}" width="1000">
-  `);
+    <button class="js_modal-close">Close</button>
+  `,
+    {
+      closable: true,
+      onShow: instance => {
+        instance.element().querySelector('.js_modal-close').onclick =
+          instance.close;
+      },
+    },
+  );
   modal.show();
 };
 
