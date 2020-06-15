b422ff5bc4eddb946734f836c6319266c6085e19
Mavr1 <mavrodiy.m.e@gmail.com>
2020-06-15 17:37:47 +0300

    Updates

---
+++ precache-manifest.39f648279b862de3882f9a4561619e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c962224c12947595d9da805fb602f328",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "43517f6fa107dd8f4fb3",
+    "url": "/goit-fe-course/static/css/main.493d0c1f.chunk.css"
+  },
+  {
+    "revision": "8a850b8c8154c6e63d3b",
+    "url": "/goit-fe-course/static/js/2.0da0ae04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-fe-course/static/js/2.0da0ae04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43517f6fa107dd8f4fb3",
+    "url": "/goit-fe-course/static/js/main.71677142.chunk.js"
+  },
+  {
+    "revision": "7a1afe4cad40ef0117b0",
+    "url": "/goit-fe-course/static/js/runtime-main.1177cb5f.js"
+  }
+]);
